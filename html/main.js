900da2b298b7c942d66a6299bb92bc3a2bb37594
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2026-04-03 01:56:19 +0100

    feat: initialize password generator project with HTML, CSS, and API-integrated JS logic

---
+++ html/main.js
@@ -0,0 +1,32 @@
+const API_URL = "https://dfifa-egor.hf.space";
+const API_KEY = "FUCK-ISREAL-76034217-LAERSI-KCUF";
+
+async function GeneratePassword() {
+    const length = document.getElementById("length").value;
+
+    try {
+        const response = await fetch(`${API_URL}?length=${length}`, {
+            method: "GET",
+            headers: {
+                "x-api-key": API_KEY
+            }
+        });
+
+        const data = await response.json();
+
+        document.getElementById("result").value = data.password;
+
+    } catch (error) {
+        alert("Error connecting to API ");
+        console.error(error);
+    }
+}
+
+function copyPassword() {
+    const result = document.getElementById("result");
+
+    result.select();
+    document.execCommand("copy");
+
+    alert("Copied! ");
+}
