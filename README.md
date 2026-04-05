aa6f61a2095985bba58842dc36b84567213f81f0
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2026-04-05 22:46:39 +0100

    feat: add database keep-alive task, configure LFS, and implement automated JS test suite

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
+---
+title: EGOR
+emoji: 🔐
+colorFrom: indigo
+colorTo: purple
+sdk: docker
+pinned: false
+---
+
 # EGOR
 
 This is the minimal setup for the EGOR application.
 
 ## Version 1 - 2026-04-03
 
-![Screenshot Version 1](assests/download%20(1).png)
+![Screenshot Version 1](assests/download(1).png)
--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, text
 # 100% BY LAADNANI
 
 # We load the URL from environment variables to bypass GitHub secret protections!
-DATABASE_URL = os.environ.get("DATABASE_URL", "mysql+pymysql://avnadmin:YOUR_AIVEN_PASSWORD_HERE@mysql-258f62ab-nonasto372-88c3.g.aivencloud.com:23661/EGOR_DB")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine (DATABASE_URL,connect_args={"ssl": {}})# creat an engine (connection) that connect to the avien database
 
