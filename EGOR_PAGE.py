eff660a95c5b0cd541b96e67a87fcbf1d127c336
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2026-04-06 21:48:39 +0100

    Serve HTML banner and add DB engine options
    Remove terminal ASCII banner module and add EGOR_PAGE.py that returns the same banner as an HTMLResponse. Update main.PY to import and return HTML_PAGE at the root endpoint and fix a logging typo. Enhance database engine in database.py by enabling pool_pre_ping and pool_recycle (300s) to improve connection reliability, with brief inline comments explaining the options.

---
+++ EGOR_PAGE.py
@@ -0,0 +1,48 @@
+from fastapi.responses import HTMLResponse
+
+# THE MAINE HTML CONVERTED TO VIA PYtHON
+
+HTML_PAGE = HTMLResponse("""
+<html>
+    <head>
+    <style>
+
+     body{
+      margin:0;
+      height:100vh;
+      display:flex;
+      justify-content: center;
+      align-items:center;
+      background:black;
+      color:rgb(243, 240, 80);
+      font-family:monospace;
+      }
+
+      pre{
+       font-size:18px;
+       text-align:center;
+       animation: glow 2s ease-in-out infinite alternate;
+       }
+
+       @keyframes glow{
+        from{
+            text-shadow: 0 0 5px #fff;
+        }
+        to{
+            text-shadow: 0 0 20px #ffd700, 0 0 30px #ffd700;
+        }
+       }
+
+    </style>
+    </head>
+    <body>
+    <pre>
+     ███████╗░██████╗░░█████╗░██████╗░
+    ██╔════╝██╔════╝░██╔══██╗██╔══██╗
+    █████╗░░██║░░██╗░██║░░██║██████╔╝
+    ██╔══╝░░██║░░╚██╗██║░░██║██╔══██╗
+    ███████╗╚██████╔╝╚█████╔╝██║░░██║
+    ╚══════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝
+    </pre>
+    </html>
+""")
--- database.py
+++ database.py
@@ -6,8 +6,11 @@ from sqlalchemy import create_engine, text
 # We load the URL from environment variables to bypass GitHub secret protections!
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine (DATABASE_URL,connect_args={"ssl": {}})# creat an engine (connection) that connect to the avien database
+engine = create_engine (DATABASE_URL,connect_args={"ssl": {}}, pool_pre_ping = True , pool_recycle = 300)
 
+# creat an engine (connection) that connect to the avien database
+# pool_pre_ping=True  : forces SQLAlchemy to transparently send a test ping (like SELECT 1) to the database every time you request a connection.
+# pool_recycle=300: This proactively replaces connection objects in the pool before they get a chance to reach timeouts on the server side (recycling every 5 minutes).
 
 def check_api_key_db(api_key : str):
     with engine.connect() as conn :
